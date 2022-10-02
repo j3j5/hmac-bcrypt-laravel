227f5f55b21afcaba154267bf245a142203c44b9
Julio Foulquie <julio@thenextweb.com>
2022-10-02 18:36:45 -0300

    Improve test suite

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -38,22 +38,6 @@ class TestCase extends \Orchestra\Testbench\TestCase
         $this->assertInstanceOf(HmacBcryptHasher::class, $this->app['hash']->driver());
     }
 
-    public function test_hash_make_returns_hash()
-    {
-        $pass = Str::random();
-        $hash = Hash::make($pass);
-
-        $this->assertNotEquals($pass, $hash);
-    }
-
-    public function test_hash_make_output_matches_hash_verify()
-    {
-        $pass = Str::random();
-        $hash = Hash::make($pass);
-
-        $this->assertTrue(Hash::check($pass, $hash));
-    }
-
     public function test_hash_check_works_as_expected_with_pregenerated_hash()
     {
         $pass = 'test-pass';
@@ -61,9 +45,82 @@ class TestCase extends \Orchestra\Testbench\TestCase
         $this->assertTrue(Hash::check($pass, $expected));
     }
 
-    public function test_hash_check_returns_false_for_empty_string()
+    public function test_hash_make_returns_hash()
     {
+        $pass = Str::random();
+        $hash = Hash::make($pass);
+
+        $this->assertNotSame($pass, $hash);
+    }
+
+    public function test_hash_make_output_validates_correctly()
+    {
+        $pass = Str::random();
+        $hash = Hash::make($pass);
+
+        $this->assertTrue(Hash::check($pass, $hash));
+    }
+
+    public function test_hash_check_returns_false_for_empty_string_hash()
+    {
+        $hashEmptyPass = Hash::make('');
         $this->assertFalse(Hash::check('', ''));
+        $this->assertFalse(Hash::check(Str::random(), ''));
+        $this->assertTrue(Hash::check('', $hashEmptyPass));
+    }
+
+    public function test_verify_algorithm_disabled_validates_other_algorithms()
+    {
+        $pass = Str::random();
+        $hash = app('hash')->driver('bcrypt')->make($pass);
+        $this->assertTrue(Hash::check($pass, $hash));
+    }
+
+    public function test_verify_algorithm_enabled_fails_to_validate_other_algorithms()
+    {
+        $this->app['config']->set('hashing.hmac-bcrypt.verify', true);
+        $pass = Str::random(75);
+        $hash = app('hash')->driver('bcrypt')->make($pass);
+
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage('This password does not use the HMAC-Bcrypt algorithm.');
+
+        Hash::check($pass, $hash);
+    }
+
+    public function test_hashing_passwords_longer_than_bcrypt_limit_works()
+    {
+        // BCrypt cuts passwords longer than 72
+        $pass1 = Str::random(75);
+        $pass2 = substr($pass1, 0, strlen($pass1) - 3) . Str::random(3);
+        $hash1 = app('hash')->driver('bcrypt')->make($pass1);
+        $hash2 = app('hash')->driver('bcrypt')->make($pass2);
+        $this->assertTrue(app('hash')->driver('bcrypt')->check($pass1, $hash1));
+        $this->assertTrue(app('hash')->driver('bcrypt')->check($pass2, $hash2));
+        // Bcrypt will validate pass1 with hash2 and viceversa
+        $this->assertTrue(app('hash')->driver('bcrypt')->check($pass1, $hash2));
+        $this->assertTrue(app('hash')->driver('bcrypt')->check($pass2, $hash1));
+
+        // Now let's try with HMAC-Bcrypt
+        $hash1 = Hash::make($pass1);
+        $hash2 = Hash::make($pass2);
+        $this->assertTrue(Hash::check($pass1, $hash1));
+        $this->assertTrue(Hash::check($pass2, $hash2));
+        // HMAC-Bcrypt will NOT validate pass1 with hash2 or the other way around
+        $this->assertFalse(Hash::check($pass1, $hash2));
+        $this->assertFalse(Hash::check($pass2, $hash1));
+
+        // Now let's go nuts
+        $pass1 = Str::random(1024);
+        $pass2 = substr($pass1, 0, strlen($pass1) - 16) . Str::random(16);
+        // Now let's try with HMAC-Bcrypt
+        $hash1 = Hash::make($pass1);
+        $hash2 = Hash::make($pass2);
+        $this->assertTrue(Hash::check($pass1, $hash1));
+        $this->assertTrue(Hash::check($pass2, $hash2));
+        // HMAC-Bcrypt will NOT validate pass1 with hash2
+        $this->assertFalse(Hash::check($pass1, $hash2));
+        $this->assertFalse(Hash::check($pass2, $hash1));
     }
 
     public function test_hash_needs_rehash_returns_false_if_no_changes()
@@ -79,7 +136,9 @@ class TestCase extends \Orchestra\Testbench\TestCase
         $pass = Str::random();
         $hash = Hash::make($pass);
 
-        $this->assertTrue(Hash::needsRehash($hash, ['rounds' => config('hashing.hmac-bcrypt.rounds') + 1]));
+        $this->assertTrue(Hash::needsRehash($hash, [
+            'rounds' => $this->app['config']->get('hashing.hmac-bcrypt.rounds') + 1
+        ]));
     }
 
     public function test_hash_needs_rehash_returns_true_for_different_algo()
@@ -93,7 +152,7 @@ class TestCase extends \Orchestra\Testbench\TestCase
     public function test_hash_needs_rehash_returns_true_for_different_algo_same_id()
     {
         $pass = Str::random();
-        $hash = crypt($pass, '$2a$07$usesomesillystringfors');
+        $hash = crypt($pass, '$2a$07$usesomesillystringforsalt$');
 
         $this->assertTrue(Hash::needsRehash($hash));
     }
@@ -126,6 +185,7 @@ class TestCase extends \Orchestra\Testbench\TestCase
         $this->expectExceptionMessage('Invalid number of rounds');
 
         $hash = Hash::make($pass, ['rounds' => 3]);
+        $hash = Hash::make($pass, ['rounds' => 32]);
     }
 
     public function test_changing_pepper_on_runtime_changes_output()
@@ -188,5 +248,25 @@ class TestCase extends \Orchestra\Testbench\TestCase
         $hash = Hash::make($pass);
 
         $this->assertTrue(Hash::check($pass, $hash));
+    }
+
+    public function test_info_function_works_for_different_inputs()
+    {
+        $pass = Str::random();
+
+        $hash = Hash::make($pass);
+        $info = Hash::info($hash);
+        $this->assertSame(HmacBcryptHasher::ALGO_NAME, $info['algoName']);
+
+        $bcryptHash = app('hash')->driver('bcrypt')->make($pass);
+        $infoBcrypt = Hash::info($bcryptHash);
+        $this->assertSame('bcrypt', $infoBcrypt['algoName']);
+
+        $argonHash = app('hash')->driver('argon')->make($pass);
+        $infoArgon = Hash::info($argonHash);
+        $this->assertSame(PASSWORD_ARGON2I, $infoArgon['algoName']);
+
+        $infoInvalid = Hash::info('$2y$07$usesomesillystringfors');
+        $this->assertSame('unknown', $infoInvalid['algoName']);
     }
 }
